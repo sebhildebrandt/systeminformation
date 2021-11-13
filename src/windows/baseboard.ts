07044500d07fd89bb9b276bd578fa1ceb453c7e9
Alexis Tyler <xo@wvvw.me>
2021-11-13 20:25:07 +1030

    refactor: windows baseboard
    Signed-off-by: Alexis Tyler <xo@wvvw.me>

--- src/windows/baseboard.ts
+++ src/windows/baseboard.ts
@@ -1,39 +1,33 @@
-import { cloneObj, getValue, nextTick, toInt } from '../common';
-import { powerShell } from '../common/exec';
-import { initBaseboard } from '../common/defaults';
+import { nextTick } from '../../common';
+import { powerShell } from '../../common/exec';
+
+interface WindowsBaseboard {
+  Manufacturer: string;
+  Product: string;
+  Model: string;
+  Version: string;
+  SerialNumber: string;
+  PartNumber: string;
+  SKU: string;
+}
 
 export const windowsBaseboard = async () => {
-  const result = cloneObj(initBaseboard);
-  try {
-    const workload = [];
-    workload.push(powerShell('Get-WmiObject Win32_baseboard | fl *'));
-    workload.push(powerShell('Get-WmiObject Win32_physicalmemoryarray | select MaxCapacity, MemoryDevices | fl'));
-    const data = await Promise.allSettled(workload).then(results => results.map(result => result.status === 'fulfilled' ? result.value : null));
-    let lines = data[0] ? data[0].toString().split('\r\n') : [''];
+  const baseboard = await powerShell('Get-WmiObject Win32_baseboard | ConvertTo-Json -Depth 5').then(data => JSON.parse(data) as WindowsBaseboard);
+  const { MaxCapacity, MemoryDevices } = await powerShell('Get-WmiObject Win32_physicalmemoryarray | select MaxCapacity, MemoryDevices | ConvertTo-Json')
+    .then(data => JSON.parse(data) as { MaxCapacity: number, MemoryDevices: number});
 
-    result.manufacturer = getValue(lines, 'manufacturer', ':');
-    result.model = getValue(lines, 'model', ':');
-    if (!result.model) {
-      result.model = getValue(lines, 'product', ':');
-    }
-    result.version = getValue(lines, 'version', ':');
-    result.serial = getValue(lines, 'serialnumber', ':');
-    result.assetTag = getValue(lines, 'partnumber', ':');
-    if (!result.assetTag) {
-      result.assetTag = getValue(lines, 'sku', ':');
-    }
-
-    // memphysical
-    lines = data[1] ? data[1].toString().split('\r\n') : [''];
-    result.memMax = toInt(getValue(lines, 'MaxCapacity', ':')) || null;
-    result.memSlots = toInt(getValue(lines, 'MemoryDevices', ':')) || null;
-
-  } catch { }
-  return result;
+  return {
+    manufacturer: baseboard.Manufacturer,
+    model: baseboard.Model ?? baseboard.Product,
+    version: baseboard.Version,
+    serial: baseboard.SerialNumber,
+    assetTag: baseboard.PartNumber ?? baseboard.SKU,
+    memMax: MaxCapacity,
+    memSlots: MemoryDevices
+  };
 };
 
 export const baseboard = async () => {
   await nextTick();
   return windowsBaseboard();
 };
-
