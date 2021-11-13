c18c34e7b171224276225262e5422a894c2aa720
Alexis Tyler <xo@wvvw.me>
2021-11-14 06:29:21 +1030

    refactor: windows mem-layout
    Signed-off-by: Alexis Tyler <xo@wvvw.me>

--- src/windows/mem-layout.ts
+++ src/windows/mem-layout.ts
@@ -1,40 +1,43 @@
-import { getValue, nextTick, toInt } from '../common';
-import { MemLayoutData } from '../common/types';
-import { powerShell } from '../common/exec';
+import { nextTick } from '../../common';
+import { powerShell } from '../../common/exec';
+
+interface WindowsPhysicalMemory {
+  Capacity: number;
+  BankLabel: string;
+  MemoryType: number;
+  SMBIOSMemoryType: number;
+  DataWidth: number;
+  TotalWidth: number;
+  ConfiguredClockSpeed: number;
+  Speed: number;
+  FormFactor: number;
+  Manufacturer: string;
+  PartNumber: number;
+  SerialNumber: string;
+  ConfiguredVoltage: number;
+  MinVoltage: number;
+  MaxVoltage: number;
+}
+
+const memoryTypes = 'Unknown|Other|DRAM|Synchronous DRAM|Cache DRAM|EDO|EDRAM|VRAM|SRAM|RAM|ROM|FLASH|EEPROM|FEPROM|EPROM|CDRAM|3DRAM|SDRAM|SGRAM|RDRAM|DDR|DDR2|DDR2 FB-DIMM|Reserved|DDR3|FBD2|DDR4|LPDDR|LPDDR2|LPDDR3|LPDDR4'.split('|');
+const formFactors = 'Unknown|Other|SIP|DIP|ZIP|SOJ|Proprietary|SIMM|DIMM|TSOP|PGA|RIMM|SODIMM|SRIMM|SMD|SSMP|QFP|TQFP|SOIC|LCC|PLCC|BGA|FPBGA|LGA'.split('|');
 
 export const windowsMemLayout = async () => {
-  const result: MemLayoutData[] = [];
-  try {
-    const memoryTypes = 'Unknown|Other|DRAM|Synchronous DRAM|Cache DRAM|EDO|EDRAM|VRAM|SRAM|RAM|ROM|FLASH|EEPROM|FEPROM|EPROM|CDRAM|3DRAM|SDRAM|SGRAM|RDRAM|DDR|DDR2|DDR2 FB-DIMM|Reserved|DDR3|FBD2|DDR4|LPDDR|LPDDR2|LPDDR3|LPDDR4'.split('|');
-    const FormFactors = 'Unknown|Other|SIP|DIP|ZIP|SOJ|Proprietary|SIMM|DIMM|TSOP|PGA|RIMM|SODIMM|SRIMM|SMD|SSMP|QFP|TQFP|SOIC|LCC|PLCC|BGA|FPBGA|LGA'.split('|');
-
-    const stdout = await powerShell('Get-WmiObject Win32_PhysicalMemory | fl *');
-    const devices = stdout.toString().split(/\n\s*\n/);
-    devices.shift();
-    devices.forEach(function (device) {
-      const lines = device.split('\r\n');
-      const dataWidth = toInt(getValue(lines, 'DataWidth', ':'));
-      const totalWidth = toInt(getValue(lines, 'TotalWidth', ':'));
-      const size = parseInt(getValue(lines, 'Capacity', ':'), 10) || 0;
-      if (size) {
-        result.push({
-          size,
-          bank: getValue(lines, 'BankLabel', ':'), // BankLabel
-          type: memoryTypes[parseInt(getValue(lines, 'MemoryType', ':'), 10) || parseInt(getValue(lines, 'SMBIOSMemoryType', ':'), 10)],
-          ecc: dataWidth && totalWidth ? totalWidth > dataWidth : false,
-          clockSpeed: parseInt(getValue(lines, 'ConfiguredClockSpeed', ':'), 10) || parseInt(getValue(lines, 'Speed', ':'), 10) || 0,
-          formFactor: FormFactors[parseInt(getValue(lines, 'FormFactor', ':'), 10) || 0],
-          manufacturer: getValue(lines, 'Manufacturer', ':'),
-          partNum: getValue(lines, 'PartNumber', ':'),
-          serialNum: getValue(lines, 'SerialNumber', ':'),
-          voltageConfigured: (parseInt(getValue(lines, 'ConfiguredVoltage', ':'), 10) || 0) / 1000.0,
-          voltageMin: (parseInt(getValue(lines, 'MinVoltage', ':'), 10) || 0) / 1000.0,
-          voltageMax: (parseInt(getValue(lines, 'MaxVoltage', ':'), 10) || 0) / 1000.0,
-        });
-      }
-    });
-  } catch { }
-  return result;
+  const devices = await powerShell('Get-WmiObject Win32_PhysicalMemory | ConvertTo-Json -Depth 5').then(data => JSON.parse(data) as WindowsPhysicalMemory[]);
+  return devices.map(device => ({
+    size: device.Capacity,
+    bank: device.BankLabel,
+    type: memoryTypes[device.MemoryType || device.SMBIOSMemoryType],
+    ecc: device.DataWidth && device.TotalWidth ? device.TotalWidth > device.DataWidth : false,
+    clockSpeed: device.ConfiguredClockSpeed || device.Speed || 0,
+    formFactor: formFactors[device.FormFactor || 0],
+    manufacturer: device.Manufacturer,
+    partNum: device.PartNumber,
+    serialNum: device.SerialNumber,
+    voltageConfigured: (device.ConfiguredVoltage || 0) / 1000.0,
+    voltageMin: (device.MinVoltage || 0) / 1000.0,
+    voltageMax: (device.MaxVoltage || 0) / 1000.0,
+  }));
 };
 
 export const memLayout = async () => {
