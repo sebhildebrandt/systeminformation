d760a2ccece7902bc5594424d5884f2a1b628a91
Sebastian Hildebrandt <hildebrandt@plus-innovations.com>
2022-01-21 08:54:57 +0100

    users tty improvement (windows)

--- test/ci.js
+++ test/ci.js
@@ -5,7 +5,7 @@ const testWithTimeout = async (fn) => {
     (async () => {
       const timeout = setTimeout(() => {
         reject('Test Timeout');
-      }, 60000);
+      }, 80000);
       const result = await fn();
       clearTimeout(timeout);
       return resolve(result);
