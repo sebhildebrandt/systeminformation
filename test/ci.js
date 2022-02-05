565f8c61e0714d8fa8a95b76a7b9ba53b94e2e5c
Sebastian Hildebrandt <hildebrandt@plus-innovations.com>
2022-02-05 12:25:22 +0100

    GitHub Action tests with time output

--- test/ci.js
+++ test/ci.js
@@ -15,49 +15,96 @@ const testWithTimeout = async (fn) => {
 
 (async () => {
   try {
+    const startTotalTime = Date.now();
+    let startTime = startTotalTime;
     console.log('Testing system:');
     console.log(await testWithTimeout(si.system));
+    console.log('----------------------------------------');
+    console.log(`Time to complete: ${(Date.now() - startTime) * 0.001} s`); startTime = Date.now();
+    console.log('----------------------------------------\n');
 
     console.log('Testing cpu:');
     console.log(await testWithTimeout(si.cpu));
+    console.log('----------------------------------------');
+    console.log(`Time to complete: ${(Date.now() - startTime) * 0.001} s`); startTime = Date.now();
+    console.log('----------------------------------------\n');
 
     console.log('Testing mem:');
     console.log(await testWithTimeout(si.mem));
+    console.log('----------------------------------------');
+    console.log(`Time to complete: ${(Date.now() - startTime) * 0.001} s`); startTime = Date.now();
+    console.log('----------------------------------------\n');
 
     console.log('Testing memLayout:');
     console.log(await testWithTimeout(si.memLayout));
+    console.log('----------------------------------------');
+    console.log(`Time to complete: ${(Date.now() - startTime) * 0.001} s`); startTime = Date.now();
+    console.log('----------------------------------------\n');
 
     console.log('Testing graphics:');
     console.log(await testWithTimeout(si.graphics));
+    console.log('----------------------------------------');
+    console.log(`Time to complete: ${(Date.now() - startTime) * 0.001} s`); startTime = Date.now();
+    console.log('----------------------------------------\n');
 
     console.log('Testing diskLayout:');
     console.log(await testWithTimeout(si.diskLayout));
+    console.log('----------------------------------------');
+    console.log(`Time to complete: ${(Date.now() - startTime) * 0.001} s`); startTime = Date.now();
+    console.log('----------------------------------------\n');
 
     console.log('Testing osInfo:');
     console.log(await testWithTimeout(si.osInfo));
+    console.log('----------------------------------------');
+    console.log(`Time to complete: ${(Date.now() - startTime) * 0.001} s`); startTime = Date.now();
+    console.log('----------------------------------------\n');
 
     console.log('Testing networkInterfaces:');
     console.log(await testWithTimeout(si.networkInterfaces));
+    console.log('----------------------------------------');
+    console.log(`Time to complete: ${(Date.now() - startTime) * 0.001} s`); startTime = Date.now();
+    console.log('----------------------------------------\n');
 
     console.log('Testing networkInterfaceDefault:');
     console.log(await testWithTimeout(si.networkInterfaceDefault));
+    console.log('----------------------------------------');
+    console.log(`Time to complete: ${(Date.now() - startTime) * 0.001} s`); startTime = Date.now();
+    console.log('----------------------------------------\n');
 
     console.log('Testing time:');
     console.log(await testWithTimeout(si.time));
+    console.log('----------------------------------------');
+    console.log(`Time to complete: ${(Date.now() - startTime) * 0.001} s`); startTime = Date.now();
+    console.log('----------------------------------------\n');
 
     console.log('Testing currentLoad:');
     console.log(await testWithTimeout(si.currentLoad));
+    console.log('----------------------------------------');
+    console.log(`Time to complete: ${(Date.now() - startTime) * 0.001} s`); startTime = Date.now();
+    console.log('----------------------------------------\n');
 
     console.log('Testing cpuTemperature:');
     console.log(await testWithTimeout(si.cpuTemperature));
+    console.log('----------------------------------------');
+    console.log(`Time to complete: ${(Date.now() - startTime) * 0.001} s`); startTime = Date.now();
+    console.log('----------------------------------------\n');
 
     console.log('Testing UUID:');
     console.log(await testWithTimeout(si.uuid));
+    console.log('----------------------------------------');
+    console.log(`Time to complete: ${(Date.now() - startTime) * 0.001} s`); startTime = Date.now();
+    console.log('----------------------------------------\n');
 
-    console.log('Testing versions:');
+    console.log('Testing Versions:');
     console.log(await testWithTimeout(si.versions));
+    console.log('----------------------------------------');
+    console.log(`Time to complete: ${(Date.now() - startTime) * 0.001} s`); startTime = Date.now();
+    console.log('----------------------------------------\n');
 
     console.log('All tests complete.');
+    console.log('----------------------------------------');
+    console.log(`Time to complete: ${(Date.now() - startTotalTime) * 0.001} s`);
+    console.log('----------------------------------------\n');
     process.exit(0);
   } catch (e) {
     console.log(e);
